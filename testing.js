2387e7c88ceb1b8d85ce14bbae911c2b913cc007
Ernesto Mancebo <ernesmancebo@gmail.com>
2013-11-20 15:55:48 -0400

    start to train

---
+++ testing.js
@@ -0,0 +1,7 @@
+// EPS
+
+// usuario = "loginname"
+// Password= "loginpassword"
+// btn 	= "loginBtn"
+
+document.getElementById('loginname').value = ' ';document.getElementById('loginpassword').value='';document.getElementById('loginBtn').click();
